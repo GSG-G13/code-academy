56f1ca222d3925c99b817327bb903c6cb47eae4e
karamzomlot <zomlutk@gmail.com>
2023-06-22 10:48:43 +0300

    feat: add router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import { styled } from 'styled-components';
+import { RouterProvider } from 'react-router-dom';
 import Providers from './Providers';
+import router from './router';
 
-function App() {
-  return (
-    <Providers>
-      <Container>
-        <Title>Happy Hacking with React & TypeScript 🚀</Title>
-      </Container>
-    </Providers>
-  );
-}
-
-const Container = styled.div`
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.primary}
-`;
+const App = () => (
+  <Providers>
+    <RouterProvider router={router} />
+  </Providers>
+);
 
 export default App;
--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,12 +2,10 @@ import { ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './constants';
 
-function Providers({ children }: { children: ReactNode }) {
-  return (
-    <ThemeProvider theme={theme}>
-      {children}
-    </ThemeProvider>
-  );
-}
+const Providers = ({ children }: { children: ReactNode }) => (
+  <ThemeProvider theme={theme}>
+    {children}
+  </ThemeProvider>
+);
 
 export default Providers;
