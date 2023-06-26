45ccf2df4d48a993812b383f548c96c64b374cf5
= <shathaamin63@gmail.com>
2023-06-26 10:39:49 +0300

    fix: fix conflict

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { theme } from './constants';
 
+const queryClient = new QueryClient();
 const Providers = ({ children }: { children: ReactNode }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </QueryClientProvider>
 );
 
 export default Providers;
