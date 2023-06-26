12701b8d0525ade4eb680fe5deb311dc1670a8cc
= <shathaamin63@gmail.com>
2023-06-26 11:37:01 +0300

    Revert "Merge remote-tracking branch 'origin' into 10-creat-profile-page"
    This reverts commit 826d553f43f630d6a7c10b96efd19d8a163a5b73.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,11 +1,9 @@
-import {
-  Members,
-  Login,
-  ActivityPage,
-  CohortsPage,
-  MainPage,
-  LikesPage,
-  ProfileInfoPage,
-} from './user/index';
+<<<<<<< HEAD
+import { ActivityPage, CohortsPage, MainPage, LikesPage, ProfileInfoPage } from './user/indext';
 
-export { MainPage, CohortsPage, ActivityPage, LikesPage, ProfileInfoPage, Members, Login };
+export { MainPage, CohortsPage, ActivityPage, LikesPage, ProfileInfoPage };
+=======
+import { Members, Login } from './user';
+
+export { Members, Login };
+>>>>>>> 3271b20f402fbc50b621fba533ffd4e33343286b
--- src/pages/user/index.ts
+++ src/pages/user/index.ts
@@ -1,10 +1,4 @@
 import Members from './Members';
 import Login from './Login';
 
-import ActivityPage from './Profile/ActivityPage';
-import CohortsPage from './Profile/CohortsPage';
-import ProfileInfoPage from './Profile/ProfileInfoPage';
-import LikesPage from './Profile/LikesPage';
-import MainPage from './Profile/MainPage';
-
-export { ActivityPage, CohortsPage, ProfileInfoPage, LikesPage, MainPage, Members, Login };
+export { Members, Login };
