5e229511cc305d4fec6654fd9150bac86cc2528b
= <shathaamin63@gmail.com>
2023-07-02 11:56:43 +0300

    fix: fix n

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,12 +1,24 @@
 import { ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
 import { theme } from './constants';
 
 const queryClient = new QueryClient();
 const Providers = ({ children }: { children: ReactNode }) => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    <ThemeProvider theme={theme}>
+      {children}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
