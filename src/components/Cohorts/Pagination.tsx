fdea4db28817f21d400ab664774d987a1fc7c563
= <shathaamin63@gmail.com>
2023-07-03 17:06:49 +0300

    feat: add pagination in front

---
+++ src/components/Cohorts/Pagination.tsx
@@ -0,0 +1,31 @@
+import { Grid } from '@mui/material';
+import Pagination from '@mui/material/Pagination';
+
+const BasicPagination = ({
+  pages,
+  currentPage,
+  setCurrentPage,
+}: {
+  pages: string;
+  currentPage: string;
+  setCurrentPage: (page: string) => void;
+}) => {
+  const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page.toString());
+  };
+
+  return (
+    <Grid container sx={{ marginTop: '40px', marginBottom: '40px', justifyContent: 'center' }}>
+      <Grid item sm={6} lg={12} md={6} xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Pagination
+          count={+pages}
+          page={+currentPage}
+          onChange={handlePageChange}
+          color="primary"
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default BasicPagination;
