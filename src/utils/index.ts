ea4d78f1f3a981ee058030ce5ff56cf4e67fe225
= <shathaamin63@gmail.com>
2023-07-03 17:06:13 +0300

    feat: add types

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 import loginSchema from './validation';
+import { Cohort, CohortData, CohortPagination, CohortsContextProps, ReqError } from './types';
 
+export type { loginSchema, Cohort, CohortData, CohortPagination, CohortsContextProps, ReqError };
 export default loginSchema;
