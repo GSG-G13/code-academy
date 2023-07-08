e26eb93659324fea009ba4960f2f55a8f370d196
= <shathaamin63@gmail.com>
2023-07-08 15:59:06 +0300

    fix: delete  isLoading

--- src/components/Cohorts/CohortsTopBar/LoadingButton.tsx
+++ src/components/Cohorts/CohortsTopBar/LoadingButton.tsx
@@ -10,44 +10,29 @@ interface IProps {
   handleClick: () => void;
 }
 
-const LoadingBtn = ({ btnTitle, btnStartIcon, cohortsCount, handleClick }: IProps) => {
-  const [loading, setLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
-  return (
-    <Box sx={{ '& > button': { m: 0 } }}>
-      <LoadingButton
-        sx={{
-          backgroundColor: '#4E64DD',
-          borderColor: 'rgba(0,0,0,0)',
-          padding: '0.5rem 1rem',
-          fontFamily: 'Poppins',
-          textTransform: 'capitalize',
-          fontWeight: '600',
-          opacity: '1',
-          borderRadius: '0.3rem',
-          position: 'relative',
-        }}
-        onClick={() => handleClick()}
-        startIcon={btnStartIcon}
-        loading={loading}
-        loadingPosition="start"
-        variant="contained"
-      >
-        <p>{btnTitle}</p>
-        <MembersCountBox>{cohortsCount}</MembersCountBox>
-      </LoadingButton>
-    </Box>
-  );
-};
+const LoadingBtn = ({ btnTitle, btnStartIcon, cohortsCount, handleClick }: IProps) => (
+  <Box sx={{ '& > button': { m: 0 } }}>
+    <LoadingButton
+      sx={{
+        backgroundColor: '#4E64DD',
+        borderColor: 'rgba(0,0,0,0)',
+        padding: '0.5rem 1rem',
+        fontFamily: 'Poppins',
+        textTransform: 'capitalize',
+        fontWeight: '600',
+        opacity: '1',
+        borderRadius: '0.3rem',
+        position: 'relative',
+      }}
+      onClick={() => handleClick()}
+      startIcon={btnStartIcon}
+      loadingPosition="start"
+      variant="contained"
+    >
+      <p>{btnTitle}</p>
+      <MembersCountBox>{cohortsCount}</MembersCountBox>
+    </LoadingButton>
+  </Box>
+);
 
 export default LoadingBtn;
