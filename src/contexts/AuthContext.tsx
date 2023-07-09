85640eae0865458227d11f22c0122d09f3f79aaf
Nour-Agha99 <temoagha@gmail.com>
2023-07-09 12:35:46 +0300

    fix: handle error toast

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { AuthProviderPropsType } from '../utils/helpers';
 import useAuth from '../hooks';
 
@@ -10,10 +11,16 @@ const RequireAuthProvider: React.FC<AuthProviderPropsType> = ({ children }) => {
     return <div>Loading...</div>;
   }
 
-  if (!user.id || user.isAdmin) {
+  if (!user.id) {
     document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
+    toast.error('you have Not Access');
     return <Navigate to="/academy/login/" />;
+  }
+
+  if (user.isAdmin) {
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    toast.error('Try to login from admin page');
+    return <Navigate to="/admin/login/" />;
   }
 
   return children;
