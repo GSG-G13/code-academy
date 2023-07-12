23d86918dec914982374e3b4d0bc92b49126baef
Batrawi <batrawi2000forever@gmail.com>
2023-07-12 12:59:06 +0300

    refactor: delete image validate

--- src/utils/validation/postSchema.ts
+++ src/utils/validation/postSchema.ts
@@ -5,13 +5,5 @@ const postSchema = z.object({
     .string()
     .nonempty('Post cannot be empty')
     .min(2, 'Post must be at least 2 characters long'),
-  postImage: z
-    .string()
-    .refine((value) => /\.(jpe?g|png|gif)$/gi.test(value), {
-      message: 'Invalid image format. Only JPEG, PNG, and GIF are supported.',
-    })
-    .optional()
-    .nullable(),
 });
-
 export default postSchema;
