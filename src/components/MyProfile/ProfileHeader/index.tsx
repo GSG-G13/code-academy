a9466fb2ca67c73798ffd11b4810b0e52486dc38
Nour-Agha99 <temoagha@gmail.com>
2023-07-16 16:20:01 +0300

    feat: connect profile info

--- src/components/MyProfile/ProfileHeader/index.tsx
+++ src/components/MyProfile/ProfileHeader/index.tsx
@@ -1,22 +1,62 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { useState } from 'react';
 import { HeaderWrapper, UserCardDiv, UserInfoDiv } from './style';
 import LabTabs from '../Tabs';
+import { profileRoutes } from '../../../services';
+import UserInfoContext from '../../../contexts/UserInfoContext';
+import { UserInfo } from '../../../utils';
 
-const ProfileHeader = () => (
-  <>
-    <HeaderWrapper>
-      <UserCardDiv>
-        <Link to="/academy/my-profile">
-          <img src="/public/GSG.webp" alt="UserImg" />
-        </Link>
-      </UserCardDiv>
-      <UserInfoDiv>
-        <h2>Khaled Al Nabaheen</h2>
-        <p>Student</p>
-      </UserInfoDiv>
-    </HeaderWrapper>
-    <LabTabs />
-  </>
-);
+interface APIResponse<T> {
+  data: T;
+}
+
+interface ProfileResponse {
+  userInfo: UserInfo;
+}
+
+const ProfileHeader = () => {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  const { isLoading } = useQuery<APIResponse<ProfileResponse>>(
+    ['profile'],
+    async () => {
+      const response = await profileRoutes.getAll();
+      return response.data as unknown as APIResponse<ProfileResponse>;
+    },
+    {
+      onSuccess: (data) => {
+        setUserInfo(data.data.userInfo);
+      },
+    },
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!userInfo) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <HeaderWrapper>
+        <UserCardDiv>
+          <Link to="/academy/my-profile">
+            <img src={userInfo.avatar} alt={userInfo.username} />
+          </Link>
+        </UserCardDiv>
+        <UserInfoDiv>
+          <h2>{userInfo.full_name}</h2>
+          <p>{userInfo.role}</p>
+        </UserInfoDiv>
+      </HeaderWrapper>
+      <UserInfoContext.Provider value={userInfo}>
+        <LabTabs />
+      </UserInfoContext.Provider>
+    </>
+  );
+};
 
 export default ProfileHeader;
