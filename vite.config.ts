66746c98626812cfe5f60bc3bd52a31f1e2f0be9
karamzomlot <zomlutk@gmail.com>
2023-07-16 10:45:47 +0300

    refactor: return to localhost endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://18.209.14.70:3000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       },
     },
